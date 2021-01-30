16c75071113cc2eeb4958086131964406765b5c6
Julian Esteban Vega <you@example.com>
2021-01-30 12:56:02 -0500

    tablas en js

---
+++ clases html/comomandar.js
@@ -0,0 +1,9 @@
+//git status
+//git add .
+//git commit -m "x"
+//push origin master
+//copiar link en setting
+//copiar dentro de la tare tanto, tareas html, y "objeto"
+//copiar el nombre de el documento
+//todo en la pagina nueva esa copiada del link de sttings
+
